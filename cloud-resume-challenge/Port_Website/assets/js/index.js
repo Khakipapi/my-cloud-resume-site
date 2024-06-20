63762eab8635c16413eef7155620baf0c8124996
Jose Reyes <jose.reyesfabian@gmail.com>
2024-06-20 19:28:26 -0400

    Delete cloud-resume-challenge directory

--- cloud-resume-challenge/Port_Website/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-//JavaScript Code 
-const counter = document.querySelector(".counter-number");
-async function updateCounter(){
-    let response = await fetch("https://zkpgcwuukc45xgjuzll3lm3wqa0xfygf.lambda-url.us-east-1.on.aws/");
-    let data =await response.json();
-    counter.innerHTML = ` Number of visitors : ${data}`;
-}
-updateCounter();
